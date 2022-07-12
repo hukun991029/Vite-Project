809e06ca525554b0cae8d5b3e0c5f881bda855f8
HuKun <1228836483@qq.com>
2022-07-12 11:21:04 +0800

    fix: 完成趋势标记组件封装

--- src/views/home/trend-marker.vue
+++ src/views/home/trend-marker.vue
@@ -1,5 +1,12 @@
 <script lang="ts" setup></script>
 <template>
-    <TrendMarker />
+    <div class="container">
+        <TrendMarker text="营业额"> </TrendMarker>
+        <TrendMarker text="销售额" type="down" downIconColor="green" :reverseColor="true" />
+    </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    display: flex;
+}
+</style>
